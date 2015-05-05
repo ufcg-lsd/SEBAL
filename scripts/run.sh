17b050f9e335d0ab62ecfbddd70b54c3675d0ac9
giovannifs <giovanni@lsd.ufcg.edu.br>
2015-05-05 14:51:58 -0300

    Updating run script for accepting additional library path from home user

--- scripts/run.sh
+++ scripts/run.sh
@@ -29,8 +29,18 @@ LOWER_Y=$5
 NUMBER_OF_PARTITIONS=$6
 PARTITION_INDEX=$7
 
+ADDITIONAL_LIBRARY_PATH=$8
+LIBRARY_PATH=/usr/local/lib/
+
+MTL_FILE=`ls $IMAGE_NAME/*MTL*`
+
+if [ -n "$ADDITIONAL_LIBRARY_PATH" ]
+then
+    LIBRARY_PATH=$LIBRARY_PATH:$ADDITIONAL_LIBRARY_PATH
+fi
+
 MTL_FILE=`ls $IMAGE_NAME/*MTL*`
 
 OUTPUT_FILE="output_execution_partition_"$PARTITION_INDEX
 
-java -Djava.library.path=/usr/local/lib/ -cp target/SEBAL-0.0.1-SNAPSHOT.jar:target/lib/* org.fogbowcloud.sebal.BulkMain $MTL_FILE $LEFT_X $UPPER_Y $RIGHT_X $LOWER_Y $NUMBER_OF_PARTITIONS $PARTITION_INDEX > $OUTPUT_FILE
+java -Djava.library.path=$LIBRARY_PATH -cp target/SEBAL-0.0.1-SNAPSHOT.jar:target/lib/* org.fogbowcloud.sebal.BulkMain $MTL_FILE $LEFT_X $UPPER_Y $RIGHT_X $LOWER_Y $NUMBER_OF_PARTITIONS $PARTITION_INDEX > $OUTPUT_FILE
