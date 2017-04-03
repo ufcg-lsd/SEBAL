037f7e8552cfa0b424ed5cc245f03701d39368cc
EsdrasVP <esdras015@gmail.com>
2017-04-03 17:56:19 -0300

    Changing WeatherStation to use NOAA API

--- src/main/java/org/fogbowcloud/sebal/parsers/WeatherStation.java
+++ src/main/java/org/fogbowcloud/sebal/parsers/WeatherStation.java
@@ -1,5 +1,6 @@
 package org.fogbowcloud.sebal.parsers;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -36,11 +37,16 @@ import org.json.JSONObject;
 
 public class WeatherStation {
 
+	private static final String NOAA_URL_PREFIX = "noaa_url_prefix";
+	private static final String NOAA_TMP_TOKEN = "noaa_tmp_token";
+	private static final String NOAA_WND_TOKEN = "noaa_wnd_token";
+	private static final String NOAA_DEW_TOKEN = "noaa_dew_token";
+
 	private static final String SEP = "--------------------";
 	private static final double R = 6371; // km
 	private static final long A_DAY = 1000 * 60 * 60 * 24;
 	
-	private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("dd-MM-YYYY");
+	private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("YYYYMMdd");
 	private static final SimpleDateFormat DATE_TIME_FORMAT = new SimpleDateFormat("dd/MM/YYYY;hhmm");
 	
 	private static final Logger LOGGER = Logger.getLogger(WeatherStation.class);
@@ -126,7 +132,7 @@ public class WeatherStation {
 		
 		HttpGet homeGet = new HttpGet(
 				"http://www.inmet.gov.br/projetos/rede/pesquisa/inicio.php");
-		httpClient.execute(homeGet);		
+		httpClient.execute(homeGet);
 		
 		HttpPost homePost = new HttpPost(
 				"http://www.inmet.gov.br/projetos/rede/pesquisa/inicio.php");
@@ -144,7 +150,88 @@ public class WeatherStation {
 		EntityUtils.toString(homePostResponse.getEntity());
 		return httpClient;
 	}
+	
+	// TODO: this needs to be done to each variable
+	// TODO: this must verify if variable's response contains all times for that day (00:00, 12:00 and 18:00)
+	private JSONArray readNOAAStation(String id, String inicio, String fim) throws Exception {
+		
+		// url prefix example: https://www7.ncdc.noaa.gov/rest/services/values/ish
+		String noaaUrlPrefix = properties.getProperty(NOAA_URL_PREFIX);
+		String noaaTMPToken = properties.getProperty(NOAA_TMP_TOKEN);
+		String noaaWNDToken = properties.getProperty(NOAA_WND_TOKEN);
+		String noaaDEWToken = properties.getProperty(NOAA_DEW_TOKEN);
+		JSONArray dataArray = new JSONArray();
+		
+		readWeatherVariable(id, "TMP", inicio, fim, noaaUrlPrefix, noaaTMPToken, dataArray);
+		readWeatherVariable(id, "WND", inicio, fim, noaaUrlPrefix, noaaWNDToken, dataArray);
+		readWeatherVariable(id, "DEW", inicio, fim, noaaUrlPrefix, noaaDEWToken, dataArray);
+		
+		for (int i = 0; i < dataArray.length(); i++) {
+			JSONObject stationDataRecord = dataArray.optJSONObject(i);
+			String temp = stationDataRecord.optString("TempBulboSeco");
+			String windDir = stationDataRecord.optString("DirecaoVento");
+			String windSpeed = stationDataRecord.optString("VelocidadeVento");
+			String dew = stationDataRecord.optString("TempBulboUmido");
+			
+			if (!temp.isEmpty() && temp != null && !windDir.isEmpty()
+					&& windDir != null && !windSpeed.isEmpty()
+					&& windSpeed != null && !dew.isEmpty() && dew != null) {
+				return dataArray;
+			}
+		}
+		
+		return null;
+	}
 
+	private void readWeatherVariable(String id, String weatherVar,
+			String inicio, String fim, String noaaUrlPrefix, String noaaToken,
+			JSONArray dataArray) throws Exception {
+		String url = noaaUrlPrefix + File.separator + id + "099999"
+				+ File.separator + weatherVar + File.separator + inicio
+				+ File.separator + fim + "?output=csv&token=" + noaaToken;
+		
+		String data = cache.get(url);
+		
+		if (data == null) {
+			try {
+				HttpGet dataGet = new HttpGet(url);
+				HttpResponse dataResponse = httpClient.execute(dataGet);
+				data = EntityUtils.toString(dataResponse.getEntity());
+				cache.put(url, data);
+			} catch (Exception e) {
+				cache.put(url, "FAILED");
+				LOGGER.error("Setting URL " + url + " as FAILED.");
+				throw e;
+			}
+		} else if (data.equals("FAILED")) {
+			throw new Exception();
+		}
+		
+		if(data != null && !data.isEmpty()) {			
+			String[] meta = data.split("\n");
+			
+			for (int i = 0; i < meta.length; i++) {
+				JSONObject jsonObject = new JSONObject();
+				String[] lineSplit = meta[i].split(",");
+				
+				if (lineSplit[2].equals(inicio)) {
+					jsonObject.put("Estacao", lineSplit[0]);
+					jsonObject.put("Data", lineSplit[2]);
+					jsonObject.put("Hora", lineSplit[3]);
+					if(weatherVar.equals("TMP")) {					
+						jsonObject.put("TempBulboSeco", lineSplit[5]);
+					} else if(weatherVar.equals("WND")) {
+						jsonObject.put("DirecaoVento", lineSplit[5]);
+						jsonObject.put("VelocidadeVento", lineSplit[8]);
+					} else if(weatherVar.equals("DEW")) {
+						jsonObject.put("TempBulboUmido", lineSplit[5]);
+					}
+				}
+				dataArray.put(jsonObject);
+			}
+		}
+	}
+	
 	private JSONArray readStation(HttpClient httpClient, String id, String inicio, String fim)
 			throws Exception {
 		String url = "http://www.inmet.gov.br/projetos/rede/pesquisa/gera_serie_txt.php?"
@@ -187,7 +274,6 @@ public class WeatherStation {
 		
 		for (int i = 0; i < dataArray.length(); i++) {
 			JSONObject stationDataRecord = dataArray.optJSONObject(i);
-//			System.out.println(stationDataRecord);
 			String temp = stationDataRecord.optString("TempBulboSeco");
 			String vel = stationDataRecord.optString("VelocidadeVento");
 			
@@ -198,7 +284,6 @@ public class WeatherStation {
 		
 		cache.put(url, "FAILED");
 		throw new Exception();
-//		return dataArray;
 	}
 
 	private JSONObject findClosestRecord(Date date, List<JSONObject> stations) {
@@ -230,12 +315,63 @@ public class WeatherStation {
 				
 				return closestRecord;
 			} catch (Exception e) {
-//				LOGGER.error("Error while reading station.", e);
-//				return null;
+				LOGGER.error("Error while reading station.", e);
 			}
 		}
 		return null;
 		
+	}
+	
+	private String readFullNOAARecord(Date date, List<JSONObject> stations, int numberOfDays) {
+		Date inicio = new Date(date.getTime() - numberOfDays * A_DAY);
+		Date fim = new Date(date.getTime() + numberOfDays * A_DAY);
+		
+		for (JSONObject station : stations) {
+			try {
+				JSONArray stationData = readNOAAStation(station.optString("id"), DATE_FORMAT.format(inicio),
+						DATE_FORMAT.format(fim));
+
+				JSONObject closestRecord = null;
+				Long smallestDiff = Long.MAX_VALUE;
+
+				for (int i = 0; i < stationData.length(); i++) {
+					JSONObject stationDataRecord = stationData.optJSONObject(i);
+					String dateValue = stationDataRecord.optString("Data");
+					String timeValue = stationDataRecord.optString("Hora");
+
+					Date recordDate = DATE_TIME_FORMAT.parse(dateValue + ";"
+							+ timeValue);
+					long diff = Math.abs(recordDate.getTime() - date.getTime());
+					if (diff < smallestDiff) {
+						smallestDiff = diff;
+						closestRecord = stationDataRecord;
+					}
+					
+					if (!closestRecord.optString("Data").isEmpty()
+							&& !closestRecord.optString("Hora").isEmpty()
+							&& !closestRecord.optString("TempBulboSeco")
+									.isEmpty()
+							&& !closestRecord.optString("TempBulboUmido")
+									.isEmpty()
+							&& !closestRecord.optString("VelocidadeVento")
+									.isEmpty()
+							&& Double.parseDouble(closestRecord
+									.optString("VelocidadeVento")) >= 0.3
+							&& !closestRecord.optString("DirecaoVento")
+									.isEmpty()) {
+						return generateStationData(stationData, closestRecord);
+					} else if(Double.parseDouble(closestRecord
+							.optString("VelocidadeVento")) < 0.3) {
+						closestRecord.remove("VelocidadeVento");
+						closestRecord.put("VelocidadeVento", "0.3");
+					}
+				} 
+			} catch(Exception e) {
+				LOGGER.error("Error while reading full NOAA record", e);
+			}
+		}		
+		
+		return null;
 	}
 	
 	private String readFullRecord(Date date, List<JSONObject> stations, int numberOfDays) {
@@ -445,13 +581,33 @@ public class WeatherStation {
 		return 4.0;
 	}
 	
-	public static void main(String[] args) throws URISyntaxException, HttpException, IOException {
-		new WeatherStation();
+	public String getNOAAStationData(double lat, double lon, Date date) {
+		List<JSONObject> station = findNearestStation(lat, lon);
+		return readFullRecord(date, station, 0);
 	}
 
 	public String getStationData(double lat, double lon, Date date) {
 		List<JSONObject> station = findNearestStation(lat, lon);
 		//return readClosestRecord(date, station, 0);
-		return readFullRecord(date, station, 0);
+		//return readFullRecord(date, station, 0);
+		return readFullNOAARecord(date, station, 0);
+	}
+	
+	public Properties getProperties() {
+		return properties;
+	}
+
+	public void setProperties(Properties properties) {
+		this.properties = properties;
+	}
+
+	public static void main(String[] args) throws Exception {
+		Properties properties = new Properties();
+		FileInputStream input = new FileInputStream("sebal.conf");
+		properties.load(input);
+
+		WeatherStation weatherStation = new WeatherStation();		
+		weatherStation.setProperties(properties);
+		weatherStation.readNOAAStation("82753", "19950101", "19950102");
 	}
 }
