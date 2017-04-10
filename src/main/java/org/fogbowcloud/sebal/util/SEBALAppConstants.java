73147ae9c3d9e6129153953ed9e951dab6fcd03d
EsdrasVP <esdras015@gmail.com>
2017-04-10 15:02:00 -0300

    Creating a util class SebalAppConstants

---
+++ src/main/java/org/fogbowcloud/sebal/util/SEBALAppConstants.java
@@ -0,0 +1,25 @@
+package org.fogbowcloud.sebal.util;
+
+public class SEBALAppConstants {
+	
+	// URL constants
+	public static final String PUBLIC_HTML_STATION_REPOSITORY = "public_html_station_repository";
+
+	// File constants
+	public static final String UNFORMATTED_LOCAL_STATION_FILE_PATH = "unformatted_local_station_file_path";
+	
+	// JSON constants
+	public static final String JSON_STATION_ID = "Estacao";
+	public static final String JSON_STATION_DATE = "Data";
+	public static final String JSON_STATION_TIME = "Hora";
+	public static final String JSON_STATION_LATITUDE = "Latitude";
+	public static final String JSON_STATION_LONGITUDE = "Longitude";
+	public static final String JSON_STATION_WIND_SPEED = "VelocidadeVento";
+	public static final String JSON_AIR_TEMPERATURE = "TempBulboSeco";
+	public static final String JSON_DEWPOINT_TEMPERATURE = "TempBulboUmido";
+	public static final String JSON_AVG_AIR_TEMPERATURE = "MediaTemperatura";
+	public static final String JSON_RELATIVE_HUMIDITY = "UmidadeRelativa";
+	public static final String JSON_MIN_TEMPERATURE = "TemperaturaMinima";
+	public static final String JSON_MAX_TEMPERATURE = "TemperaturaMaxima";
+	public static final String JSON_SOLAR_RADIATION = "RadiacaoSolar";
+}
