80b195030b733306dac66140b76282277ff03b6f
Lucas Farias <lucas_fnf@gmail.com>
2017-08-01 16:07:21 -0300

    Add check on NFS monitor to prevent adding empty lines

--- scripts/collect-nfs-usage.sh
+++ scripts/collect-nfs-usage.sh
@@ -5,5 +5,5 @@ TIME_BETWEEN_COMMANDS=1
 echo TIMESTAMP, READ, WRITE
 while [ -e /proc/$1 ]; do
     cmd=$(nfsiostat $TIME_BETWEEN_COMMANDS 2)
-    echo ${cmd} | awk -v date="$( date +"%s" )" '{ print date", "$67", "$85 }' 2> /dev/null
+    echo ${cmd} | awk -v date="$( date +"%s" )" '{ if (NF > 87) print date", "$67", "$85; }' 2> /dev/null
 done
