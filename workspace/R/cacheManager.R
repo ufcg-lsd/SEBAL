00f8b81790c6c292b9e5321092244d1a7127376d
Paulo Felipe <paulofelipe.feitosa@gmail.com>
2018-03-20 17:40:51 -0300

    Insert object implementation in cacheManager

--- workspace/R/cacheManager.R
+++ workspace/R/cacheManager.R
@@ -1,58 +1,89 @@
-cacheManager <- setClass(
-  "cacheManager",
-  
-  slots = c(
-    tmpObjectsPaths <- "vector",
-    tmpDirec <- "character",
-    cache <- "vector",
-    cacheSize <- "numeric"
-  ),
-  
-  prototype = c(
-    tmpObjectsPaths <- vector(),
-    tmpDirec <- "/tmp/",
-    cache <- vector(),
-    cacheSize <- 10
-  ),
-  
-  validity = function(object) {
-    if(object@cacheSize < 2) {
-      return("Invalid Cache Size")
+library(raster)
+library(pryr)
+
+CM <- list(tmpObjectsPaths = vector(), tmpDirec = "/tmp/", cacheSize = 2, cache = list(), objectNameByCache = list())
+class(CM) <- append(class(CM), "cacheManager")
+
+addObject <- function(object, rasterObject) {
+  UseMethod("addObject", object)
+}
+
+putObjectInCache <- function(object, objectRef, rasterObject, overWrite) {
+  UseMethod("putObjectInCache", object)
+}
+
+removeLastObject <- function(object, overWrite) {
+  UseMethod("removeLastObject", object)
+}
+
+cacheIsFull <- function(object) {
+  UseMethod("cacheIsFull", object)
+}
+
+addObject.cacheManager <- function(object, rasterObject) {
+  rasterObjectName <- deparse(substitute(rasterObject))
+  for(i in c(1:nlayers(rasterObject))) {
+    objectRef <- paste(rasterObjectName, i, sep = '-')
+    
+    object$tmpObjectsPaths[[objectRef]] <- paste(object$tmpDirec, objectRef, sep = '')
+    
+    object <- putObjectInCache(object, objectRef, rasterObject[[i]], TRUE)
+  }
+  rm(list = c(rasterObjectName), envir = as.environment(globalenv()))
+  return(object)
+}
+
+putObjectInCache.cacheManager <- function(object, objectRef, rasterObject, overWrite) {
+  if(cacheIsFull(object) == TRUE) {
+    object <- removeLastObject(object, overWrite)
+  }
+  object$cache <- c(object$cache, as.list(rasterObject))
+  object$objectNameByCache <- c(object$objectNameByCache, as.list(objectRef))
+  return(object)
+}
+
+removeLastObject.cacheManager <- function(object, overWrite) {
+  # think about write the raster in disk with a new thread
+  fileName <- object$tmpObjectsPaths[[ object$objectNameByCache[[1]] ]]
+  if(overWrite == TRUE) {
+    writeRaster(object$cache[[1]], filename = fileName, overwrite = TRUE)
+  } else {
+    if(!file.exists(fileName)) {
+      writeRaster(object$cache[[1]], filename = fileName)
     }
-    length(object@cache) <- object@cacheSize
+  }
+  
+  object$cache <- object$cache[c(-1)]
+  object$objectNameByCache <- object$objectNameByCache[c(-1)]
+  return(object)
+}
+
+cacheIsFull.cacheManager <- function(object) {
+  if(length(object$cache) == object$cacheSize) {
     return(TRUE)
+  } else {
+    return(FALSE)
   }
-)
+}
 
-setGeneric(
-  name = "addObject",
-  def = function(classObject, rasterObject) {
-    standardGeneric("addObject")
-  }
-)
+r1 <- raster(ncols=7000, nrows=7000)
+r1[] <- 1:ncell(r1)
+object.size(r1)
+r2 <- raster(ncols=7000, nrows=7000)
+r2[] <- (1:ncell(r2)) + 4000000
+object.size(r2)
+r3 <- raster(ncols=7000, nrows=7000)
+r3[] <- 1:ncell(r3) + 8000000
+object.size(r3)
 
-setMethod(
-  f = "addObject",
-  signature = "cacheManager",
-  definition = function(cacheManager, rasterObject) {
-    rasterObjectName <- deparse(substitute(rasterObject))
-    for(i in c(1:nlayers(rasterObject))) {
-      objectRef <- paste(rasterObjectName, i, sep = '-')
-      cacheManager@tmpObjectsPaths[[objectRef]] <- paste(cacheManager@tmpDirec, objectRef, sep = '')
-    }
-    return(cacheManager)
-  }
-)
+mem_used()
 
+CM <- addObject(CM, r1)
+CM
+CM <- addObject(CM, r2)
+CM
+CM <- addObject(CM, r3)
+CM
 
-
-test <- cacheManager(tmpDirec = '/tmp/', cacheSize = 20)
-test
-r <- raster(ncols=10, nrows=10)
-test <- addObject(test, r)
-test
-test@tmpDirec
-test@tmpObjectsPaths[['r-1']]
-
-
+mem_used()
 
